8c7817bef19b0792a4164bc116b771ac394b3a88
manikantanukala <107236759+manikantanukala@users.noreply.github.com>
2023-03-23 13:21:35 +0530

    Update bitb.sh

--- bitb.sh
+++ bitb.sh
@@ -1,3 +1,13 @@
 
 hello 
 this is a check file
+yum install wget unzip -y
+
+cd /opt
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.73/bin/apache-tomcat-9.0.73.zip
+unzip apache-tomcat-9.0.73.zip
+cd /opt/apache-tomcat-9.0.73/bin
+chmod u+x *.sh
+
+ln -s /opt/apache-tomcat-9.0.73/bin/startup.sh /usr/bin/startTomcat
+ln -s /opt/apache-tomcat-9.0.73/bin/shutdown.sh /usr/bin/stopTomcat
